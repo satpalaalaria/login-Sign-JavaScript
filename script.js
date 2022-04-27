e5b9e1d1e72a37a15b731a7fb833aa51db3297f8
SATPAL AALARIA <satpal19@navgurukul.org>
2022-04-27 10:57:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -157,6 +157,15 @@ function validate() {
     };
 };
 
+//add image
+const imgInp = document.querySelector("#imgInp");
+imgInp.onchange = (evt) => {
+  const [file] = imgInp.files;
+  if (file) {
+    blah.src = URL.createObjectURL(file);
+  }
+};
+
 
 
 
